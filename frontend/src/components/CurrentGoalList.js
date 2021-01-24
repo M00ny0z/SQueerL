8632c21999831b8c7767bda3b4d652cbd8397da9
Manny Munoz <em66@uw.edu>
2021-01-23 21:19:08 -0800

    Added fake data and renderGoalsmethod

--- frontend/src/components/CurrentGoalList.js
+++ frontend/src/components/CurrentGoalList.js
@@ -2,7 +2,46 @@ import React from 'react';
 
 import Goal from './Goal';
 
-const CuurentGoalList = () => {
-    
+const CurrentGoalList = () => {
+   const data = [{
+      name: "Walk a mile a day",
+      "add-code": "XYZ123"
+   },
+   {
+      name: "Walk the dog everyday",
+      "add-code": "ABC123"
+   },
+   {
+      name: "Go to the gym once a week",
+      "add-code": "DFG123"
+   },
+   {
+      name: "Get takeout ONLY once a week",
+      "add-code": "JFK123"
+   },
+   {
+      name: "Organize things",
+      "add-code": "DYZ123"
+   },{
+      name: "Walk a mile a day",
+      "add-code": "DOL123"
+   },
+   {
+      name: "Walk a mile a day",
+      "add-code": "THE123"
+   }];
 
-}
+   const renderGoals = () => {
+      return data.map(
+         (goal) => <Goal name={ goal.name } code={ goal.code } />
+      );
+   };
+
+   return (
+      <div>
+         { renderGoals() }
+      </div>
+   );
+};
+
+export default CurrentGoalList;
