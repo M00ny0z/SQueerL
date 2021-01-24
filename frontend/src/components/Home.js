2ff23f962dda2df031d105154fdc65e3ca74a981
Manny Munoz <em66@uw.edu>
2021-01-23 20:10:20 -0800

    Added overall Home component

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Goal from './Goal';
+const Home = () => {
+   return (
+      <div className="container">
+         <Goal name="Walk for an Hour"/>
+      </div> 
+   );
+};
+
+export default Home;
