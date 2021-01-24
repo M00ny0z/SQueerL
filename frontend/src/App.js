32a08d395ff910ab5001125b541cefe25ff60c5a
Manny Munoz <em66@uw.edu>
2021-01-23 20:10:31 -0800

    Added overall Home route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Home from './components/Home';
 import Header from './components/Header';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <div>
          <BrowserRouter>
             <Header />
+            <Route path="/" exact component={Home}/>
          </BrowserRouter>
       </div>
    );
