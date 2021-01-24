40024916aa3e890fb38481fffba421a9b14c5d96
Zihan Xue <zihan.x@foxmail.com>
2021-01-23 21:31:00 -0800

    used dropdown for 'change timeframe' button

--- frontend/src/components/Goal.js
+++ frontend/src/components/Goal.js
@@ -3,6 +3,8 @@ import '../styles/index.css';
 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 const Goal = (props) => {
    return (
@@ -12,8 +14,12 @@ const Goal = (props) => {
                <p className="text-bold">{ props.name }</p>
                <p>{ props.body }</p>
                <p> addcode: { props.addcode }</p>
-               <Button variant="primary">change timeframe</Button>
                <Button variant="link" href={"/squeerl/goal/" + props.addcode}>go to the page</Button>
+               <DropdownButton id="dropdown-basic-button" title="Change timeframe">
+                  <Dropdown.Item href="#/action-1">Once a day</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Once a week</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Every other day</Dropdown.Item>
+               </DropdownButton>
             </Card.Body>
          </Card>
       </div>
