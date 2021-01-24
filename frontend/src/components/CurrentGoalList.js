36ae9f9153ca36b8202a39517ee421a42e078ad1
Manny Munoz <em66@uw.edu>
2021-01-23 23:20:11 -0800

    Removed fake data from CurrentGoalList and now uses useGoals hook

--- frontend/src/components/CurrentGoalList.js
+++ frontend/src/components/CurrentGoalList.js
@@ -1,38 +1,13 @@
 import React from 'react';
+import useGoals from '../hooks/useGoals';
 
 import Goal from './Goal';
 
 const CurrentGoalList = () => {
-   const data = [{
-      name: "Walk a mile a day",
-      "code": "XYZ123"
-   },
-   {
-      name: "Walk the dog everyday",
-      "code": "ABC123"
-   },
-   {
-      name: "Go to the gym once a week",
-      "code": "DFG123"
-   },
-   {
-      name: "Get takeout ONLY once a week",
-      "code": "JFK123"
-   },
-   {
-      name: "Organize things",
-      "code": "DYZ123"
-   },{
-      name: "Walk a mile a day",
-      "code": "DOL123"
-   },
-   {
-      name: "Walk a mile a day",
-      "code": "THE123"
-   }];
+   const goals = useGoals();
 
    const renderGoals = () => {
-      return data.map(
+      return goals.map(
          (goal) => <Goal name={ goal.name } code={ goal.code } />
       );
    };
